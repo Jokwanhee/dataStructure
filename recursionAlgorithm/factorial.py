ed015002ee72221174437c0d078dfe2272830104
JoKwanhee <jonotch1@naver.com>
2022-01-27 11:45:07 +0900

    run time check

--- recursionAlgorithm/factorial.py
+++ recursionAlgorithm/factorial.py
@@ -1,6 +1,7 @@
 # 순환 알고리즘 - 팩토리얼
 # n! = 1 ... (n = 1)
 #    = n*(n-1)! ... (n > 1)
+import time
 
 # 팩토리얼 순환 구조, O(n)
 def factorial(n):
@@ -19,8 +20,14 @@ def factorialRepeat(n):
 
 def main():
     _input = int(input("n을 입력하시오 : "))
+    start_recursion = time.time()
     print("팩토리얼 순환 구조 :", factorial(_input))
+    end_recursion = time.time()
+    print("팩토리얼 순환 구조 소요시간 :", end_recursion - start_recursion)
+    start_repeat = time.time()
     print("팩토리얼 반복 구조 :", factorialRepeat(_input))
+    end_repeat = time.time()
+    print("팩토리얼 반복 구조 소요시간 :", end_repeat - start_repeat)
 
 if __name__ == "__main__":
     main()
--- recursionAlgorithm/power.py
+++ recursionAlgorithm/power.py
@@ -1,4 +1,6 @@
 # 순환 알고리즘 - 거듭제곱
+import time
+
 
 # 순환 거듭제곱
 def recursionPower(x, n):
@@ -9,6 +11,7 @@ def recursionPower(x, n):
     else:  # n승이 짝수
         return recursionPower(x * x, n // 2)
 
+
 # 반복 거듭제곱
 def repeatPower(x, n):
     result = 1
@@ -16,11 +19,18 @@ def repeatPower(x, n):
         result *= x
     return result
 
+
 def main():
     _inputX = int(input("x을 입력하시오 : "))
     _inputN = int(input("n을 입력하시오 : "))
+    start_recursion = time.time()
     print("순환 알고리즘 순환 거듭제곱, %d의 %d제곱 :" % (_inputX, _inputN), recursionPower(_inputX, _inputN))
+    end_recursion = time.time()
+    print("순환 알고리즘 순환 거듭제곱 소요시간 :", end_recursion - start_recursion)
+    start_repeat = time.time()
     print("순환 알고리즘 반복 거듭제곱, %d의 %d제곱 :" % (_inputX, _inputN), repeatPower(_inputX, _inputN))
+    end_repeat = time.time()
+    print("순환 알고리즘 반복 거듭제곱 소요시간 :", end_repeat - start_repeat)
 
 if __name__ == '__main__':
     main()
